bf98ca1bfb138c647a1d36600e5478cc54d43d40
Basant <btirkey1208@gmail.com>
2025-06-04 19:17:42 +0530

    changes in code

--- signLanguage/pipeline/training_pipeline.py
+++ signLanguage/pipeline/training_pipeline.py
@@ -14,26 +14,26 @@ class TrainPipeline:
     
 
 
-        def start_data_ingestion(self)-> DataIngestionArtifact:
-            try:
-                logging.info(
-                    "Entered the start_data_ingestion method of TrainPipeline class"
-                )
-                logging.info("Getting the data from URL")
+    def start_data_ingestion(self)-> DataIngestionArtifact:
+        try:
+            logging.info(
+                "Entered the start_data_ingestion method of TrainPipeline class"
+            )
+            logging.info("Getting the data from URL")
 
-                data_ingestion = DataIngestion(
-                    data_ingestion_config =  self.data_ingestion_config
-                )
-
-                data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
-                logging.info("Got the data from URL")
-                logging.info(
-                    "Exited the start_data_ingestion method of TrainPipeline class"
+            data_ingestion = DataIngestion(
+                data_ingestion_config =  self.data_ingestion_config
                 )
 
-                return data_ingestion_artifact
-            except Exception as e:
-                raise SignException(e, sys)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info("Got the data from URL")
+            logging.info(
+                "Exited the start_data_ingestion method of TrainPipeline class"
+                )
+
+            return data_ingestion_artifact
+        except Exception as e:
+         raise SignException(e, sys)
 
 
 
